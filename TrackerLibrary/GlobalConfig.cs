9d5760f782969997c3403f8b047ac52c9d6d79ce
Vlad Constantini <vlad.constantini@valoris.ro>
2021-06-17 17:05:19 +0300

    DataConnection

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if(database == true)
+            {
+                // TODO
+            }
+
+            if(textFiles)
+            {
+                // TODO
+            }
+        }
+    }
+}
